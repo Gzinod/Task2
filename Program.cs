c4fb443dfcb638a3f88791d9f4f221ecc84ef38a
GzINOD <gzinod31@rambler.ru>
2022-11-15 21:08:09 +0300

    T

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+ Console.WriteLine("input num1");
+ int num1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("input num2");
+ int num2 = Convert.ToInt32(Console.ReadLine());
+ if (num1 % num2 == 0)
+ {
+    Console.WriteLine("OK");
+ }
+ else Console.WriteLine($"остаток{num1 %num2}");
